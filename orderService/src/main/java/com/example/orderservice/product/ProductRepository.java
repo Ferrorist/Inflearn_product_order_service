6b36ad7b8d5858f3ff8585aed4c3129245f180de
Ferrorist <ferrorist@naver.com>
2024-08-13 23:26:38 +0900

    feat: JPA 적용

--- orderService/src/main/java/com/example/orderservice/product/ProductRepository.java
+++ orderService/src/main/java/com/example/orderservice/product/ProductRepository.java
@@ -3,16 +3,10 @@ package com.example.orderservice.product;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-class ProductRepository {
+interface ProductRepository extends JpaRepository<Product, Long>{
 
-    private Long sequence = 0L;
-    private Map<Long, Product> persistence = new HashMap<>();
-
-    public void save(Product product) {
-        product.assignId(++sequence);
-        persistence.put(product.getId(), product);
-    }
 }
